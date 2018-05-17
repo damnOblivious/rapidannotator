bada316893fc67f5c7ec19b6b1bc42cbcb7424fc
guptavaibhav18197 <guptavaibhav18197@gmail.com>
2018-05-18 01:28:14 +0530

    Added routes and forms for frontpage module

---
+++ rapidannotator/modules/frontpage/forms.py
@@ -0,0 +1,96 @@
+"""Forms for Login/Registeration."""
+
+from flask_wtf import FlaskForm
+from flask_login import current_user
+from flask_babelex import lazy_gettext as _
+from sqlalchemy.orm.exc import NoResultFound
+from flask_security.forms import email_required, email_validator, \
+    unique_user_email
+from wtforms import FormField, PasswordField, StringField, SubmitField, \
+    BooleanField
+from wtforms.validators import DataRequired, EqualTo, StopValidation, \
+    ValidationError, Email
+
+from rapidannotator.models import User
+from rapidannotator.validators import USERNAME_RULES, validate_username
+
+
+def strip_filter(text):
+    """Filter for trimming whitespace.
+
+    :param text: The text to strip.
+    :returns: The stripped text.
+    """
+    return text.strip() if text else text
+
+class LoginForm(FlaskForm):
+    """Form for users to login."""
+
+    username = StringField(
+        label=_('Username'),
+        validators=[DataRequired(message=_('Username not provided.'))],
+        filters=[strip_filter],)
+
+    password = PasswordField(
+        label=_('Password'),
+        validators=[DataRequired(message=_('Password not provided.'))],)
+
+    remember_me = BooleanField(_('Remember Me'))
+
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        label=_('Username'),
+        description=_('Required. %(username_rules)s',
+                      username_rules=USERNAME_RULES),
+        validators=[DataRequired(message=_('Username not provided.'))],
+        filters=[strip_filter],
+    )
+
+    fullname = StringField(
+        label=_('Fullname'),
+        filters=[strip_filter],
+    )
+
+    email = StringField(
+        label=_('Email'),
+        # validators=[DataRequired(message=_('Email not provided.')), Email()]
+    )
+
+    password = PasswordField(
+        label=_('Password'),
+        validators=[DataRequired(message=_('Username not provided.'))],)
+
+    password2 = PasswordField(
+        label=_('Confirm Password'),
+        validators=[DataRequired(message=_('Confirmation password not provided.')),
+                    EqualTo('password')])
+
+    def validate_username(self, username):
+        """Wrap username validator for WTForms."""
+        try:
+            validate_username(username.data)
+        except ValueError as e:
+            raise ValidationError(e)
+
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError(_('Please use a different username.'))
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError(_('Email address already registered.'))
+
+class DummyForm(FlaskForm):
+    """A Dummy form for testing and debugging."""
+
+    username = StringField(_('Enter something random'),)
+    submit = SubmitField(_('Sign In'))
+
+class DummyForm2(FlaskForm):
+    """A Dummy form for testing and debugging."""
+
+    username = StringField(_('Enter something random From 2'),)
+    email = StringField(_('Email'),)
+    password = PasswordField(_('Password'),)
+    submit = SubmitField(_('Sign In'))
---
+++ rapidannotator/modules/frontpage/views.py
@@ -0,0 +1,69 @@
+from flask import render_template, flash, redirect, url_for, request, jsonify, \
+    current_app, g
+from flask_babelex import lazy_gettext as _
+from flask_login import current_user, login_required
+from flask_login import login_user, logout_user, current_user
+
+from rapidannotator import db
+from rapidannotator import bcrypt
+from rapidannotator.models import User
+from rapidannotator.modules.frontpage import blueprint
+from rapidannotator.modules.frontpage.forms import LoginForm
+from rapidannotator.modules.frontpage.forms import LoginForm, RegistrationForm
+
+@blueprint.route('/')
+def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('home.index'))
+    loginForm = LoginForm()
+    registrationForm = RegistrationForm()
+    return render_template('frontpage/main.html',
+        loginForm = loginForm,
+        registrationForm = registrationForm)
+
+@blueprint.route('/login', methods=['POST'])
+def login():
+    loginForm = LoginForm()
+    registrationForm = RegistrationForm()
+
+    if loginForm.validate_on_submit():
+        user = User.query.filter_by(username=loginForm.username.data).first()
+        if user is None or not bcrypt.check_password_hash(
+                                user.password, loginForm.password.data):
+            flash(_('Invalid username or password'))
+            return render_template('frontpage/main.html',
+                loginForm = loginForm,
+                registrationForm = registrationForm)
+
+        login_user(user, remember=loginForm.remember_me.data)
+        return "logged in successful"
+
+    return render_template('frontpage/main.html',
+        loginForm = loginForm,
+        registrationForm = registrationForm)
+
+@blueprint.route('/register', methods=['POST'])
+def register():
+    if current_user.is_authenticated:
+        return "already logged in! :) :) "
+    loginForm = LoginForm()
+    registrationForm = RegistrationForm()
+
+    if registrationForm.validate_on_submit():
+        hashedPassword = bcrypt.generate_password_hash(
+            registrationForm.password.data).decode('utf-8')
+
+        user = User(
+            username=registrationForm.username.data,
+            fullname=registrationForm.fullname.data,
+            email=registrationForm.email.data,
+            password=hashedPassword
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash(_('Congratulations, you are now a registered user!'))
+        return "KOOK"
+
+    return render_template('frontpage/main.html',
+        loginForm = loginForm,
+        registrationForm = registrationForm)
