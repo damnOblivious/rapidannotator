4430632bb3fd0e6165429d320f922064cf9ba03f
guptavaibhav18197 <guptavaibhav18197@gmail.com>
2018-05-18 01:43:20 +0530

    Added some temporary files

---
+++ someUsefulCommands.py
@@ -0,0 +1,16 @@
+'''
+
+import sys
+from rapidannotator import app
+app.logger.info("heri fera")
+app.logger.info(hashedPassword)
+
+----------------------------------------------------
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'SQLALCHEMY_DATABASE_URI', 'sqlite:///test.db'
+)
+
+
+
+'''
