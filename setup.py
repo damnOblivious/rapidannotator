04426a5d4176b387842907a22a70f0fef3c3ba16
guptavaibhav18197 <guptavaibhav18197@gmail.com>
2018-05-18 01:11:34 +0530

    Setup: added setup files

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='rapidannotator',
+    packages=['rapidannotator'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+    ],
+)
+
+'''
+sudo apt-get install python3-mysqldb
+bcrypt
+passlib
+sudo -H pip3 install flask-sqlalchemy flask-security
+'''
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+import sys
+sys.path.insert(0, '/home/tissues/interests/lal/rapidannotator')
+
+from rapidannotator import app as application
