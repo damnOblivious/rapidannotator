7d389f6b09a5061b18b70b73bccedf0be1ced971
guptavaibhav18197 <guptavaibhav18197@gmail.com>
2018-05-22 02:27:09 +0530

    Added authentication login required feature

--- rapidannotator/modules/home/views.py
+++ rapidannotator/modules/home/views.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, redirect, url_for, request, jsonify, \
-    current_app, g
+    current_app, g, abort
 from flask_babelex import lazy_gettext as _
 
 from rapidannotator import db
@@ -10,11 +10,24 @@ from rapidannotator import bcrypt
 from flask_login import current_user, login_required
 from flask_login import login_user, logout_user, current_user
 
+'''
+@blueprint.before_request
+def before_request():
+    if not current_user.is_authenticated:
+        return current_app.login_manager.unauthorized()
+'''
+
+@blueprint.before_request
+@login_required
+def before_request():
+    pass
+
+
 @blueprint.route('/')
 def index():
     return render_template('home/main.html')
 
-@blueprint.route('/logout', methods=['GET','POST'])
+@blueprint.route('/logout', methods=['POST'])
 def logout():
     logout_user()
     return redirect(url_for('frontpage.index'))
