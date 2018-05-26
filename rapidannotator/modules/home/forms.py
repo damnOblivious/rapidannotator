fabfd2283612edaa7415bde75131f354272f253a
guptavaibhav18197 <guptavaibhav18197@gmail.com>
2018-05-27 04:12:44 +0530

    Added form for creating new experiment

--- rapidannotator/modules/home/forms.py
+++ rapidannotator/modules/home/forms.py
@@ -6,11 +6,12 @@ from flask_security.forms import email_required, email_validator, \
     unique_user_email
 from flask_wtf import FlaskForm
 from sqlalchemy.orm.exc import NoResultFound
-from wtforms import FormField, PasswordField, StringField, SubmitField, BooleanField
+from wtforms import FormField, PasswordField, StringField, SubmitField, \
+    BooleanField, SelectField
 from wtforms.validators import DataRequired, EqualTo, StopValidation, \
-    ValidationError, Email
+    ValidationError, Email, Length
 
-from rapidannotator.models import User
+from rapidannotator.models import Experiment
 
 
 def strip_filter(text):
@@ -20,6 +21,35 @@ def strip_filter(text):
     :returns: The stripped text.
     """
     return text.strip() if text else text
+
+class AddExperimentForm(FlaskForm):
+    name = StringField(
+        label=_('Experiment Name'),
+        description=_("Required. Experiment name can't exceed 40 characters"),
+        validators=[DataRequired(message=_('Experiment name not provided.')),
+                    Length(max=40 ,message=_("Experiment name can't exceed 40 characters."))],
+        filters=[strip_filter],
+    )
+
+    description = StringField(
+        label=_('Experiment description'),
+        description=_("A short description, can't exceed 320 characters"),
+        validators=[Length(max=320 ,message=_("Experiment description can't exceed 320 characters."))],
+        filters =[strip_filter],
+    )
+
+    category = SelectField(
+        label=_('Type of experiment'),
+        description=_("Select the type of files that your experiment has."),
+        choices=[   ('text', 'text'),
+                    ('image', 'image'),
+                    ('audio', 'audio'),
+                    ('video', 'video')],
+    )
+    def validate_name(self, name):
+        experiment = Experiment.query.filter_by(name=name.data).first()
+        if experiment is not None:
+            raise ValidationError(_('Please use a different username.'))
 
 class DummyForm(FlaskForm):
     """A Dummy form for testing and debugging."""
