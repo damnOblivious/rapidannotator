d27b4e76e6726ca87b4330c5d002dc3e5c78f0b9
guptavaibhav18197 <guptavaibhav18197@gmail.com>
2018-08-05 04:47:18 +0530

    removed code for view_experiment package

--- rapidannotator/modules/view_experiment/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-blueprint = Blueprint(
-        'view_experiment',
-        __name__,
-        template_folder='templates',
-)
-
-from rapidannotator.modules.view_experiment import views
